d5b86599a0638785155208b5db82f78877f75cb7
john jairo rodriguez medina <johnjairorodriguez384@gmail.com>
2022-08-19 16:32:22 -0500

    restart, develop branch is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,10 @@
 import React from "react";
 import "./css/defaultCss/App.css";
 
-import { useFetch } from "./hooks/useFetch";
-
-import { Loader } from "./components/Loader/Loader";
-import { GetCharacters } from "./components/Characters/GetCharacters";
-
 const App = () => {
-  /*se realiza una prueba, en un momento se procede a borrar la misma*/
-  const [data, loading, error] = useFetch(
-    "https://jsonplaceholder.typicode.com/users"
-  );
 
   return (
     <div className="App">
-      <header className="App-header">
-        <GetCharacters/>
-        {/* se deja el condicional del data.lenght por seguridad de carga*/}
-        {/*en el momento en que data.length sea true, simplemente la condicion no se cumple y el loader pasa a false*/}
-        {loading && data.length === 0 ? (
-          <div>
-            <Loader font={80} />
-          </div>
-        ) : (
-          <ul>
-            {data.map((user: any) => (
-              <li key={user.id}>{user.name}</li>
-            ))}
-          </ul>
-        )}
-      </header>
     </div>
   );
 };
--- src/components/Characters/GetCharacters.tsx
+++ src/components/Characters/GetCharacters.tsx
@@ -1,9 +1,6 @@
-/*import { useState, useEffect } from "react";*/
-
-import { useFetch } from "../../hooks/useFetch";
 
 export const GetCharacters = () => {
-  const [dataA, loadingA, errorA, setDataA] = useFetch("https://rickandmortyapi.com/api/character");
+
   return (
     <div>
       hola
