7efd787fcaf76ef2de0fd27abf029058ca36e4e1
john jairo rodriguez medina <johnjairorodriguez384@gmail.com>
2022-08-20 23:44:07 -0500

    save

--- src/components/Characters/GetCharacters.tsx
+++ src/components/Characters/GetCharacters.tsx
@@ -3,19 +3,38 @@ import { useState, useEffect } from "react";
 import { fetchData } from "../../helpers/fetchData";
 
 export const GetCharacters = (): any => {
-  const [data, setData] = useState<any>(null);
+/*  const datos = [
+    { name: "john", id: 1 },
+    { name: "pepe", id: 2 },
+    { name: "pae", id: 3 },
+  ];*/
+  const [data, setData] = useState<any>([]);
+  //* manejo de data provisional, manejando de manera visual momentanea
+  /*  const [dataUI, setDataUI] = useState<any>(null); */
   useEffect(() => {
-    fetchData("https://rickandmortyapi.com/api/character",setData);
+    fetchData("https://rickandmortyapi.com/api/character", setData);
   }, []);
+
+  const dataFilter = (id: string | number) => {
+    console.log(id);
+    const dataFilter = data.filter((el: any) =>  el.id !== id);
+    /*console.log("hola");*/
+    console.log(dataFilter)
+    setData(dataFilter);
+  };
 
   return (
     <div>
       <>
-      {data?.results.map((el:any)=> {
-        return (
-          <li key = {el.id}>{el.name}</li>
-        )
-      })}
+        {data.length > 0 && data.map((el: any) => {
+          const { id } = el;
+          return (
+            <div key={el.name}>
+              <li>{el.name}</li>
+              <button onClick={() => dataFilter(id)}>X</button>
+            </div>
+          );
+        })}
       </>
     </div>
   );
--- src/helpers/fetchData.ts
+++ src/helpers/fetchData.ts
@@ -1,8 +1,14 @@
-export const fetchData = async (url: string, setD: Function) => {
+export const fetchData = async (
+  url: string,
+  setD: Function,
+  setD2: Function = () => {}
+) => {
   try {
     const res = await fetch(url);
-    const json = await res.json();
-    setD(json);
+    const { results } = await res.json();
+
+    setD(results);
+    setD2(results); 
   } catch (err) {
     console.log(err);
   }
