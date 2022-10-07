cca13c04724c6a3eee65869a2bc952b63c29a86f
john jairo rodriguez medina <johnjairorodriguez384@gmail.com>
2022-10-06 23:46:32 -0500

    .

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,5 @@
+
+
 import React from "react";
 import { HeaderWithAuth } from "../../atomos/Header/HeaderWithAuth";
 import { GetCharacters } from "../../components/Characters/GetCharacters";
@@ -6,9 +8,24 @@ import { AuthView } from "../AuthView";
 export const Home = (props: any) => {
   return (
     <div>
-      {
-        props.children
-      }
+     {/* <CryptoDonate
+        cryptoDomain="<insert-domain>.crypto"
+        infuraApi="<insert-infura-api>"
+        colors={{
+          primary: "#2096f3",
+          secondary: "#fde199",
+          button: "#1a78c2",
+          buttonSecondary: "#fab601",
+          text: "#ffffff",
+        }}
+        text={{
+          title: "Donate",
+          thanks: "Thank you for donating, see the below link for transaction",
+          copied: "Address copied, please donate via wallet",
+        }}
+      />*/}
+
+      {props.children}
     </div>
   );
 };
