5f32eb8637a33208cfeaaf00ccc2849a299475c6
john jairo rodriguez medina <johnjairorodriguez384@gmail.com>
2022-11-28 11:24:09 -0500

    npm i

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,4 +1,5 @@
-import {FC, useEffect, lazy, useState} from 'react';
-//lazy loading 
-  
-//routes of my project
+import { FC, useEffect, lazy, useState } from "react";
+//lazy loading
+
+//Browser routers
+import { BrowserRouter as Router, Route } from "react-router-dom";
